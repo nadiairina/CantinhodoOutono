c63c721582c7139532342b0b052ad581ae6fcc5d
nadiairina <nadiairina934@gmail.com>
2025-09-16 14:43:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const functions = require("firebase-functions");
-const stripe = require("stripe")(functions.config().stripe.key);
+const stripe = require("stripe")(functions.config().stripe.secret);
 
 // Função para criar uma intenção de pagamento no Stripe
 exports.createPaymentIntent = functions.https.onCall(async (data, context) => {
-  try {
-    const items = data.items;
+    try {
+        const { items } = data; // Espaços removidos aqui
 
-    if (!items || items.length === 0) {
-      throw new functions.https.HttpsError('invalid-argument', 'O carrinho está vazio.');
-    }
+        if (!items || items.length === 0) {
+            throw new functions.https.HttpsError('invalid-argument', 'O carrinho está vazio.');
+        }
 
-    // Calcular o valor total com base nos itens e no envio
-    const SHIPPING_COST = 5.00;
-    const totalAmount = items.reduce((total, item) => {
-      return total + (item.price * item.quantity);
-    }, 0);
+        // Calcular o valor total com base nos itens e no envio
+        const SHIPPING_COST = 5.00;
+        const totalAmount = items.reduce((total, item) => {
+            return total + (item.price * item.quantity);
+        }, 0);
 
-    const finalAmount = totalAmount + SHIPPING_COST;
+        const finalAmount = totalAmount + SHIPPING_COST;
 
-    // Criar a intenção de pagamento com o Stripe
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: Math.round(finalAmount * 100),
-      currency: "eur",
-    });
+        // Criar a intenção de pagamento com o Stripe
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: Math.round(finalAmount * 100),
+            currency: "eur",
+        });
 
-    // Retornar a chave secreta de cliente para o frontend
-    return {
-      clientSecret: paymentIntent.client_secret,
-    };
-  } catch (error) {
-    console.error("Erro ao criar o Payment Intent:", error);
-    throw new functions.https.HttpsError('unknown', 'Não foi possível criar o Payment Intent.', error.message);
-  }
+        // Retornar a chave secreta de cliente para o frontend
+        return {
+            clientSecret: paymentIntent.client_secret,
+        };
+    } catch (error) {
+        console.error("Erro ao criar o Payment Intent:", error);
+        throw new functions.https.HttpsError('unknown', 'Não foi possível criar o Payment Intent.', error.message);
+    }
 });
